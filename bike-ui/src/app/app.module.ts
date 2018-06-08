c2410aaa6a94ecbefaa016f59f525631939bc2c0
Oleh Soklakov <oleh_soklakov@epam.com>
2018-06-08 16:46:40 +0300

    Add Bike Service in Angular app

--- bike-ui/src/app/app.module.ts
+++ bike-ui/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BikeService} from "./services/bike.service";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BikeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ bike-ui/src/app/services/bike.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type' : 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BikeService {
+
+  constructor(private httpClient:HttpClient) { }
+
+  getBikes() {
+    return this.httpClient.get('/server/api/v1/bikes');
+  }
+}
